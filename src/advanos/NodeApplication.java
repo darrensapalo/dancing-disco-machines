bc9307dca77288c597fea6c9b8ed829a79f4d132
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2014-11-19 20:00:58 +0800

    Null checking.

--- src/advanos/NodeApplication.java
+++ src/advanos/NodeApplication.java
@@ -78,7 +78,7 @@ public class NodeApplication {
 	}
 
 	public synchronized void createNextTCPconnection(Host newHost) {
-		if (next.equals(newHost)) return;
+		if (next != null && next.equals(newHost)) return;
 		next = newHost;
 		
 		if (nextHandler != null){
